d4db897eb99439718ca53d974ed6fc868582fd4b
ilya777grin23 <IRuss23@yandex.ru>
2018-07-15 05:31:20 +0300

    onresize upg

---
+++ scripts/resize-controll.js
@@ -0,0 +1,4 @@
+window.onresize = () => {
+    document.querySelector('.burger-menu').classList.remove('menu_active');
+    document.querySelector('.screen-navigation').classList.remove('active_screen');
+}
